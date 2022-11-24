f46871178eb7fb510ac17ad6f4264838cc3b93fc
Dilmurod <di.murod.1999@gmail.com>
2022-11-24 16:49:55 +0500

    now game is recoverable using sessionStorage

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,88 @@ import NavigationTop from "./components/NavigationTop";
 import Playground from "./containers/Playground";
 import Game from './containers/Game';
 
+const initialState = {
+  started: false,
+  confirmed: false,
+  startMove: false,
+  player: "1",
+  firstPlayerData: [],
+  secondPlayerData: []
+}
+
 export default class App extends Component {
   state = {
-    started: false,
-    confirmed: false,
-    startMove: false,
-    player: "1",
-    firstPlayerData: [],
-    secondPlayerData: []
+    ...initialState
   };
 
+  componentDidMount() {
+    const navalCombat = sessionStorage.getItem('navalCombat') || '{}';
+
+    if (navalCombat !== '{}') {
+      this.setState(JSON.parse(navalCombat));
+    } else {
+      this.setState(initialState)
+      sessionStorage.setItem('navalCombat', JSON.stringify(initialState));
+    }
+  }
+
   startHandler = () => {
-    this.setState({ started: true });
+    const navalCombat = {
+      ...JSON.parse(sessionStorage.getItem('navalCombat') || '{}'),
+      started: true
+    }
+    this.setState(navalCombat);
+    sessionStorage.setItem('navalCombat', JSON.stringify(navalCombat))
   };
 
   resetHandler = () => {
-    this.setState({
+    const navalCombat = {
       started: false,
       confirmed: false,
       startMove: false,
       player: "1",
       firstPlayerData: [],
       secondPlayerData: []
-    })
+    }
+    this.setState(navalCombat)
+    sessionStorage.setItem('navalCombat', JSON.stringify(navalCombat))
   }
 
   confirmData = (userFields: Array<string>) => {
+    const navalCombat = JSON.parse(sessionStorage.getItem('navalCombat') || '{}');
     if (this.state.player === '1') {
       this.setState({ firstPlayerData: userFields, player: '2' })
+      sessionStorage.setItem(
+        'navalCombat',
+        JSON.stringify({ ...navalCombat, firstPlayerData: userFields, player: '2' })
+      )
     } else {
       this.setState({ secondPlayerData: userFields, player: '1', confirmed: true });
+      sessionStorage.setItem(
+        'navalCombat',
+        JSON.stringify({ ...navalCombat, secondPlayerData: userFields, player: '1', confirmed: true })
+      )
     }
   };
+
+  startMoveHandler = () => {
+    const navalCombat = JSON.parse(sessionStorage.getItem('navalCombat') || '{}');
+    sessionStorage.setItem(
+      'navalCombat',
+      JSON.stringify({ ...navalCombat, startMove: true })
+    )
+    this.setState({ startMove: true })
+  }
+
+  playerChangeHandler = (player: string) => {
+    const navalCombat = JSON.parse(sessionStorage.getItem('navalCombat') || '{}');
+    sessionStorage.setItem(
+      'navalCombat',
+      JSON.stringify({ ...navalCombat, player })
+    )
+    this.setState({ player })
+  }
+
 
   render() {
     const { started, player, confirmed, startMove, firstPlayerData, secondPlayerData } = this.state;
@@ -50,8 +100,8 @@ export default class App extends Component {
             {(!confirmed && player === "2") && <Playground player="2" confirmData={this.confirmData} />}
           </div>
         )}
-        {(!startMove && confirmed) && <button className="btn btn-success" onClick={() => this.setState({ startMove: true })}>start move</button>}
-        {startMove && <Game player={player} firstPlayerData={firstPlayerData} secondPlayerData={secondPlayerData} playerChangeHandler={(player: string) => this.setState({ player })} />}
+        {(!startMove && confirmed) && <button className="btn btn-success" onClick={this.startMoveHandler}>start move</button>}
+        {startMove && <Game player={player} firstPlayerData={firstPlayerData} secondPlayerData={secondPlayerData} playerChangeHandler={this.playerChangeHandler} reset={this.resetHandler} />}
       </div>
     );
   }
--- src/containers/Game.tsx
+++ src/containers/Game.tsx
@@ -7,28 +7,74 @@ type GameProps = {
   firstPlayerData: Array<string>
   secondPlayerData: Array<string>
   playerChangeHandler: (player: string) => void
+  reset: () => void
 };
+
+interface playerAttack {
+  killed: Array<string>
+  missed: Array<string>
+}
 
 type GameState = {
   selectedField: string
   firstPlayerPoint: number
+  firstPlayerAttack: playerAttack
   secondPlayerPoint: number
+  secondPlayerAttack: playerAttack
   ended: boolean
 };
 
+const initialState = {
+  selectedField: '',
+  firstPlayerPoint: 0,
+  firstPlayerAttack: {
+    killed: [],
+    missed: []
+  },
+  secondPlayerPoint: 0,
+  secondPlayerAttack: {
+    killed: [],
+    missed: []
+  },
+  ended: false
+}
+
 class Game extends Component<GameProps, GameState> {
-  state = {
-    selectedField: '',
-    firstPlayerPoint: 0,
-    secondPlayerPoint: 0,
-    ended: false
+  state = { ...initialState }
+
+  componentDidMount() {
+    const gameObj = JSON.parse(sessionStorage.getItem('navalCombat') || '{}');
+    if (gameObj.attacked) {
+      this.setState(gameObj);
+      const secondPlayerElements = document.querySelectorAll<HTMLElement>(`.player-2 .square`);
+      secondPlayerElements.forEach(item => {
+        if (gameObj.firstPlayerAttack.killed.includes(item.dataset.id)) {
+          item.classList.add('killed');
+        } else if (gameObj.firstPlayerAttack.missed.includes(item.dataset.id)) {
+          item.classList.add('missed');
+        }
+      })
+      const firstPlayerElements = document.querySelectorAll<HTMLElement>(`.player-1 .square`);
+      firstPlayerElements.forEach(item => {
+        if (gameObj.secondPlayerAttack.killed.includes(item.dataset.id)) {
+          item.classList.add('killed');
+        } else if (gameObj.secondPlayerAttack.missed.includes(item.dataset.id)) {
+          item.classList.add('missed');
+        }
+      })
+    } else {
+      this.setState(initialState)
+      sessionStorage.setItem('navalCombat', JSON.stringify({ ...gameObj, ...initialState }));
+    }
   }
 
   clickHandler = (e: React.MouseEvent<HTMLElement>, player: string) => {
     const id = e.currentTarget.dataset.id || '';
     if (this.state.firstPlayerPoint === 8 || this.state.secondPlayerPoint === 8) {
       alert(`Game is ended! Winner is Player ${this.props.player}`);
-      this.setState({ ended: true })
+      this.setState({ ended: true });
+      const navalCombat = JSON.parse(sessionStorage.getItem('navalCombat') || '{}');
+      sessionStorage.setItem('navalCombat', JSON.stringify({ ...navalCombat, ended: true }));
       return;
     }
     if (player === this.props.player) {
@@ -50,6 +96,7 @@ class Game extends Component<GameProps, GameState> {
 
   attackHandler = () => {
     const elements = document.querySelectorAll<HTMLElement>(`.player-${this.props.player === '1' ? '2' : '1'} .square.active`);
+    const navalCombat = JSON.parse(sessionStorage.getItem('navalCombat') || '{}');
 
     if (this.props.player === '1') {
       this.setState({ selectedField: '' });
@@ -60,8 +107,19 @@ class Game extends Component<GameProps, GameState> {
             element.classList.add('killed');
           }
         });
-        this.setState((state) => ({ firstPlayerPoint: state.firstPlayerPoint + 1 }))
+
+        const attack: playerAttack = { ...this.state.firstPlayerAttack }
+        attack.killed.push(this.state.selectedField);
+        sessionStorage.setItem('navalCombat', JSON.stringify({
+          ...navalCombat,
+          attacked: true,
+          firstPlayerAttack: attack,
+          firstPlayerPoint: this.state.firstPlayerPoint + 1
+        }))
+
+        this.setState((state) => ({ firstPlayerPoint: state.firstPlayerPoint + 1, firstPlayerAttack: attack }))
         alert('Killed');
+
         if (this.state.firstPlayerPoint === 7) {
           alert(`Game is ended! Winner is Player ${this.props.player}`);
           this.setState({ ended: true })
@@ -74,6 +132,16 @@ class Game extends Component<GameProps, GameState> {
             element.classList.add('missed');
           }
         });
+
+        const attack: playerAttack = { ...this.state.firstPlayerAttack }
+        attack.missed.push(this.state.selectedField);
+        sessionStorage.setItem('navalCombat', JSON.stringify({
+          ...navalCombat,
+          attacked: true,
+          firstPlayerAttack: attack
+        }))
+        this.setState({ firstPlayerAttack: attack })
+
         alert('missed');
         this.props.playerChangeHandler('2');
       }
@@ -86,8 +154,19 @@ class Game extends Component<GameProps, GameState> {
             element.classList.add('killed');
           }
         });
+
+        const attack: playerAttack = { ...this.state.secondPlayerAttack }
+        attack.killed.push(this.state.selectedField);
+        sessionStorage.setItem('navalCombat', JSON.stringify({
+          ...navalCombat,
+          secondPlayerAttack: attack,
+          attacked: true,
+          secondPlayerPoint: this.state.secondPlayerPoint + 1
+        }))
+
         alert('Killed');
-        this.setState((state) => ({ secondPlayerPoint: state.secondPlayerPoint + 1 }))
+        this.setState((state) => ({ secondPlayerPoint: state.secondPlayerPoint + 1, secondPlayerAttack: attack }))
+
         if (this.state.secondPlayerPoint === 7) {
           alert(`Game is ended! Winner is Player ${this.props.player}`);
           this.setState({ ended: true })
@@ -100,13 +179,26 @@ class Game extends Component<GameProps, GameState> {
             element.classList.add('missed');
           }
         });
+
+        const attack: playerAttack = { ...this.state.secondPlayerAttack }
+        attack.missed.push(this.state.selectedField);
+        sessionStorage.setItem('navalCombat', JSON.stringify({
+          ...navalCombat,
+          attacked: true,
+          secondPlayerAttack: attack
+        }))
+        this.setState({ secondPlayerAttack: attack })
+
         alert('missed');
         this.props.playerChangeHandler('1');
       }
     }
   }
 
-  endHandler = () => { alert(`Game is ended! Winner is Player ${this.props.player}`); }
+  endHandler = () => {
+    alert(`Game is ended! Winner is Player ${this.props.player}`);
+    this.props.reset();
+  }
 
   render(): ReactNode {
     const { selectedField, ended, firstPlayerPoint, secondPlayerPoint } = this.state;
